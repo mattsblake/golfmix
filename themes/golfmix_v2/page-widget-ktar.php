475f5639bde7611a3013833d371396115ab8cd27
Matt Blake <matt.blake@fansided.com>
2014-09-11 08:46:23 -0700

    KTAR Widget update

--- themes/golfmix_v2/page-widget-ktar.php
+++ themes/golfmix_v2/page-widget-ktar.php
@@ -166,137 +166,21 @@ font-weight: bold;
 
 	<div class="box-holder" id="review-of-the-day">
 
-				<div class="daily-box-top"><h3>Featured Course</h3></div>
-				<div class="daily-box-bottom-2">
-							<?php 
-							
-								global $options;
-								foreach ($options as $value) {
-								    if (get_settings( $value['id'] ) === FALSE) { $$value['id'] = $value['std']; } else { $$value['id'] = get_settings( $value['id'] ); }
-								}
-								date_default_timezone_set('America/Phoenix');
-								$time = date("m/d/Y");
-								if(isset($_REQUEST['beta'])) {echo $time.$gm_date1;}
-							
-								if($time == $gm_date1) { 
-									$review_post_id = $gm_postid1;
-									$review_id = $gm_email1;
-									$prize_course_name = $gm_prizename1;
-									$prize_link = $gm_prizeurl1;
-									$date = 1;
-								} elseif($time == $gm_date2) {
-									$review_post_id = $gm_postid2;
-									$review_id = $gm_email2;
-									$prize_course_name = $gm_prizename2;
-									$prize_link = $gm_prizeurl2;
-									$date = 2;
-								}
-								elseif($time == $gm_date3) { 
-									$review_post_id = $gm_postid3;
-									$review_id = $gm_email3;
-									$prize_course_name = $gm_prizename3;
-									$prize_link = $gm_prizeurl3;
-									$date = 3;
-								}
-								
-								if(isset($_REQUEST['beta'])) {echo $date;}
-								
-								
-								if(isset($_REQUEST['beta'])) { echo $review_post_id; }
-								
-								if(!$date) {
-									if($gm_postid1) {
-										$review_post_id = $gm_postid1;
-										$review_id = $gm_email1;
-										$prize_course_name = $gm_prizename1;
-										$prize_link = $gm_prizeurl1;
-									} elseif($gm_postid2) {
-										$review_post_id = $gm_postid2;
-										$review_id = $gm_email2;
-										$prize_course_name = $gm_prizename2;
-										$prize_link = $gm_prizeurl2;
-									} elseif($gm_postid3) {
-										$review_post_id = $gm_postid3;
-										$review_id = $gm_email3;
-										$prize_course_name = $gm_prizename3;
-										$prize_link = $gm_prizeurl3;
-									}
-								}
-								
-								$image = get_bloginfo('template_url').'/images/golfmix_icon_med.jpg';
-				
-								$course_id= get_post_meta($review_post_id, 'course_id',1);
-								include('courses_call.php'); 
-				
-								global $turn_flickr_off;
-								$turn_flickr_off = null;
-								$turn_flickr_off = get_post_meta($review_post_id, 'turn_flickr_off', 1);
-								
-								global $blocked_photos;
-								$blocked_photos = explode(',',get_post_meta(get_the_ID(), 'blocked_photos', 1));
-								
-				
-								global $c_name_encode;
-								$c_name_encode = str_replace(" ", "+", $c_name);							
-								require_once($_SERVER['DOCUMENT_ROOT']."/wp-content/themes/golfmix/phpflickr/phpFlickr.php");
-								$f = new phpFlickr("37eb5032134f95f57d29b3b363d489f9");
-								$recent = $f->photos_search(array("text"=>"'".$c_name_encode."'","sort"=>"relevance", "per_page"=>"7"));
-																										
-								if($recent['total'] > 0 && $turn_flickr_off == '') {
-												
-								$pcount = 0;
-								foreach ($recent['photo'] as $photo) {
-									$pid = $photo['id'];
-									if(in_array($pid, $blocked_photos)) { continue; }
-								
-									$pcount = $pcount + 1;
-									if($pcount == 1) {
-										$pid = $photo['id'];
-										$pserver = $photo['server'];
-										$pfarm = $photo['farm'];
-										$ptitle = $photo['title'];
-										$psecret = $photo['secret'];
-										$powner = $photo['owner'];
-													
-										$puri = 'http://www.flickr.com/photos/'.$powner.'/'.$pid;
-								
-										$image = 'http://farm'.$pfarm.'.static.flickr.com/'.$pserver.'/'.$pid.'_'.$psecret.'_z.jpg';
-									}
-								}
-								} 
-								
-								$last_value = $value;
-								
-								$ratings = get_ratings($review_post_id);
-
-															
-								$args = array('status' => 'approve', 'number' => '1','order' => 'ASC','post_id' => $review_post_id, 'author_email' => $review_id );
-								$comments = get_comments($args);
-								foreach($comments as $comment) :
-							?>
+				<div class="daily-box-top"><h3>Featured Offer</h3></div>
+				<div class="daily-box-bottom-2">							
 								<div id="ktar-promo">
-									<h4><a href="<?php echo get_permalink($review_post_id); ?>" target="_blank"><?php echo get_the_title($review_post_id); ?></a></h4>
+									<h4>YOUR CHANCE TO WIN THE GOLF TRIP OF A LIFETIME AT PEBBLE BEACH!</h4>
 									<div class="best-image-wrapper">
-										<div class="best-image"><a href="<?php echo get_permalink($review_post_id); ?>" target="_blank"><img src="<?php echo $image; ?>"></a></div>
+										<div class="best-image" style="width: 100%;height: auto;"><a href="http://www.sunhealth.org/foundation/golf-dream-trip-lexus-champions-charity-2014" target="_blank"><img src="http://golfmix.com/wp-content/uploads/2014/07/lexus-champions-for-charity-logo.png" style="width:100%;"></a></div>
 									</div>
 									<div class="promo-content">
-										<ul class="ratings">
-											<li <?php if($sortby == 'value') { echo 'class="bold"'; }?>>Value:<?php echo num_to_stars($ratings["Value"]); ?></li>
-											<li <?php if($sortby == 'conditions') { echo 'class="bold"'; }?>>Course Conditions:<?php echo num_to_stars($ratings["Course Conditions"]); ?></li>
-											<li <?php if($sortby == 'design') { echo 'class="bold"'; }?>>Design:<?php echo num_to_stars($ratings["Design"]); ?></li>
-											<li <?php if($sortby == 'amenities') { echo 'class="bold"'; }?>>Amenities:<?php echo num_to_stars($ratings["Amenities"]); ?></li>
-											<li <?php if($sortby == 'pace') { echo 'class="bold"'; }?>>Pace of Play:<?php echo num_to_stars($ratings["Pace of Play"]); ?></li>
-											<li <?php if($sortby !== 'value' && $sortby !== 'conditions' && $sortby !== 'design' && $sortby !== 'amenities' && $sortby !== 'pace') { echo 'class="bold"'; }?>>Overall Experience:<?php echo num_to_stars($ratings["<strong>Overall Experience</strong>"]); ?></li>
-										</ul>
 									</div>
 									<div class="clear"></div>
 								</div>
 																
-								<div class="review-congrats">Write a review to win at <a href="<?php bloginfo('url'); ?>/write-a-review" target="_blank">Arizona'a leading golf course review site!</a></div> 
+								<div class="review-congrats">Win the golf trip of a lifetime to Pebble Beach at <a href="http://www.sunhealth.org/foundation/golf-dream-trip-lexus-champions-charity-2014" target="_blank">sunhealth.org</a></div> 
 								
-								<?php endforeach; ?>
-								
-	
+							
 							</div>
 							
 				<div class="clear"></div>
